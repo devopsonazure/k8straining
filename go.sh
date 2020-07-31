9052cda6792d53941d04de1de3c9f1efca0181a5
huangyingting <huangyingting@outlook.com>
2020-07-31 13:21:53 +0000

    nodeport plus xip.io

--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
-PUBLIC_IP=$(kubectl -n nginx-ingress get service nginx-ingress-controller | tail -n 1 | awk '{ print $4; }').xip.io
-sed 's/public-ip.xip.io/'$PUBLIC_IP'/g' -i stress.sh
-sed 's/public-ip.xip.io/'$PUBLIC_IP'/g' -i manifests/ingress.yml
+VOTE_HOST=$(kubectl -n nginx-ingress get service nginx-ingress-controller | tail -n 1 | awk '{ print $4; }').xip.io
+sed 's/vote-host.xip.io/'$VOTE_HOSTNAME'/g' -i stress.sh
+sed 's/vote-host.xip.io/'$VOTE_HOSTNAME'/g' -i manifests/ingress.yml
 kubectl create ns k8straining
 kubectl apply -f manifests/storageclass.yml
 kubectl apply -f manifests/. 
 echo ""
-echo "application is running at: http://$PUBLIC_IP"
+echo "application is running at: http://$VOTE_HOST"
