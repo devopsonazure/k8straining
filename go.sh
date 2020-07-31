9c38cb6b683e085cd67b5d75ef0265c83652b4ff
huangyingting <huangyingting@outlook.com>
2020-07-31 13:23:04 +0000

    nodeport plus xip.io

--- go.sh
+++ go.sh
@@ -1,6 +1,6 @@
 VOTE_HOST=$(kubectl -n nginx-ingress get service nginx-ingress-controller | tail -n 1 | awk '{ print $4; }').xip.io
-sed 's/vote-host.xip.io/'$VOTE_HOSTNAME'/g' -i stress.sh
-sed 's/vote-host.xip.io/'$VOTE_HOSTNAME'/g' -i manifests/ingress.yml
+sed 's/vote-host.xip.io/'$VOTE_HOST'/g' -i stress.sh
+sed 's/vote-host.xip.io/'$VOTE_HOST'/g' -i manifests/ingress.yml
 kubectl create ns k8straining
 kubectl apply -f manifests/storageclass.yml
 kubectl apply -f manifests/. 
