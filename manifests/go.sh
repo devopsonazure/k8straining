d3bfb06f2a0b5d4a346817d5249c9482fd871ffd
huangyingting <huangyingting@outlook.com>
2020-08-23 08:21:32 +0000

    update script to support addon manager

--- manifests/go.sh
+++ manifests/go.sh
@@ -1,6 +1,8 @@
 VOTE_HOST=$(kubectl -n nginx-ingress get service nginx-ingress-controller | tail -n 1 | awk '{ print $4; }').xip.io
 sed 's/vote-host.xip.io/'$VOTE_HOST'/g' -i stress.sh
 sed 's/vote-host.xip.io/'$VOTE_HOST'/g' -i ingress.yml
+sed 's/vote-host.xip.io/'$VOTE_HOST'/g' -i job.yml
+sed 's/vote-host.xip.io/'$VOTE_HOST'/g' -i cronjob.yml
 kubectl create ns k8straining
 kubectl apply -f storageclass.yml
 kubectl apply -f . 
